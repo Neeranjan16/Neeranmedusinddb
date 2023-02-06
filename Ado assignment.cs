c6fc08d57085c2c045a36cdb92fc4f9c8ce9fba4
neeranjan <nikhilneeranjan@619@gmail.com>
2023-02-06 07:53:11 +0530

    files

---
+++ Ado assignment.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ConsoleAppassignment5._1
+{
+    class Program
+    {
+        public static SqlConnection conn;
+        public static SqlCommand Cmd;
+
+        static void Main(string[] args)
+        {
+            Insert_values();
+            Console.Read();
+        }
+        static SqlConnection GetConnection()
+        {
+            conn = new SqlConnection("Data Source=DESKTOP-0IB3J5K\\SQLEXPRESS;Initial Catalog=neemedusindDB;" +
+                "Integrated Security=true;");
+            conn.Open();
+            return conn;
+        }
+
+
+        static void Insert_values()
+        {
+            conn = GetConnection();
+
+
+
+            Console.WriteLine("Emp No:");
+            int Empno = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Emp Name");
+            string EmpName = Console.ReadLine();
+
+            Console.WriteLine("Emp Salary:");
+            double EmpSal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Emp Type:");
+            Char EmpType = Convert.ToChar(Console.ReadLine());
+
+            Cmd = new SqlCommand("INSERT INTO Code_Employee VALUES(@empno, @empname, @empsal, @emptype)");
+
+
+
+
+            Cmd.Parameters.AddWithValue("@empno", Empno);
+            Cmd.Parameters.AddWithValue("@empname", EmpName);
+            Cmd.Parameters.AddWithValue("@empsal", EmpSal);
+            Cmd.Parameters.AddWithValue("@emptype", EmpType);
+            Cmd.Connection = conn;
+
+            int values = Cmd.ExecuteNonQuery();
+            if (values > 0)
+            {
+                Console.WriteLine("Employee Details are Stored");
+            }
+            else
+            {
+                Console.WriteLine("Error please contact admin");
+            }
+
+            Cmd = new SqlCommand("select * from Code_employee where emsal >=25000", conn);
+
+            conn.Close();
+
+
+
+        }
+
+    }
+}
